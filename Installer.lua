138a71c244f30cbc43ed84b4f50caf37898e7364
TheAio <76646135+TheAio@users.noreply.github.com>
2023-01-19 19:04:12 +0100

    Added the abillity to skip reading the license
    Added the ability to skip reading the license, you can now continue with the installation without reading the entire file.

--- Installer.lua
+++ Installer.lua
@@ -1,40 +1,51 @@
 print("Would you like to install CC-ALLUDE? Y/n")
 selection = read()
 if selection == "Y" then
-  shell.run("mv LICENSE LICENSE-for-another-file")
-  shell.run("wget https://raw.githubusercontent.com/TheAio/CC-ALLUDE/main/LICENSE LICENSE")
-  if fs.exists("LICENSE") then
-    file = fs.open("LICENSE","r")
-    fileData = {}
-    while true do
-      line = file.readLine()
-      if line == nil then
-        break
-      else
-        fileData[#fileData+1] = line
-      end
-    end
-    print("")
-    for line=1,#fileData do
-      print(fileData[line])
-      E,K = os.pullEvent("key")
-    end
-    print("")
-    print("If you agree to this license please press Y otherwise press N")
-    while true do
-      E,K = os.pullEvent("key")
-      if K == keys.n then
-        shell.run("rm LICENSE")
-        shell.run("mv LICENSE-for-another-file LICENSE")
-        print("License denied, aborting install")
-        return false
-      elseif K == keys.y then
-        break
-      end
-    end
+  print("To install CC-ALLUDE with this script, you need to accept the license")
+  print("it is GNU General Public License v3.0")
+  print("Press enter to read more..")
+  print("The license in short is:")
+  print("Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.")
+  print("Do you want to read the entire license or accept it directly? R/A")
+  if read() == "A" then
+    shell.run("mv LICENSE LICENSE-for-another-file")
+    shell.run("wget https://raw.githubusercontent.com/TheAio/CC-ALLUDE/main/LICENSE LICENSE")
   else
-    print("Unable to download the license, aborting install")
-    return false
+    shell.run("mv LICENSE LICENSE-for-another-file")
+    shell.run("wget https://raw.githubusercontent.com/TheAio/CC-ALLUDE/main/LICENSE LICENSE")
+    if fs.exists("LICENSE") then
+      file = fs.open("LICENSE","r")
+      fileData = {}
+      while true do
+        line = file.readLine()
+        if line == nil then
+          break
+        else
+          fileData[#fileData+1] = line
+        end
+      end
+      print("")
+      for line=1,#fileData do
+        print(fileData[line])
+        E,K = os.pullEvent("key")
+      end
+      print("")
+      print("If you agree to this license please press Y otherwise press N")
+      while true do
+        E,K = os.pullEvent("key")
+        if K == keys.n then
+          shell.run("rm LICENSE")
+          shell.run("mv LICENSE-for-another-file LICENSE")
+          print("License denied, aborting install")
+          return false
+        elseif K == keys.y then
+          break
+        end
+      end
+    else
+      print("Unable to download the license, aborting install")
+      return false
+    end
   end
   shell.run("wget https://raw.githubusercontent.com/TheAio/CC-ALLUDE/main/APM APM")
   if fs.exists("APM") then
